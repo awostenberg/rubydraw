6784c78c6a4e9b7e7eb0f60bfe06075c5eae3e14
J. Wostenberg <awostenberg@gmail.com>
2011-11-03 16:32:50 -0600

    now you can open a blank window

--- lib/rubydraw.rb
+++ lib/rubydraw.rb
@@ -1,1 +1,9 @@
 require 'ruby-sdl-ffi'
+
+          # So I don't have to type "rubydraw/filename_here" all the time
+def rbd_require(file)
+  require "rubydraw/#{file}"
+end
+
+          # Require all the rubydraw files
+rbd_require "window"
