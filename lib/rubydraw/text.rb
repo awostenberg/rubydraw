e5f6620fd10a008a96c985407f191031733d9116
J. Wostenberg <awostenberg@gmail.com>
2011-11-19 22:11:03 -0700

    Added a drawable text class and a few default fonts

--- lib/rubydraw/text.rb
+++ lib/rubydraw/text.rb
@@ -3,11 +3,22 @@ module Rubydraw
   # with the text to display. They can be drawn at a position on the
   # screen.
   class Text
-    attr_accessor(:font, :contents, :size, :color)
+    attr_accessor(:contents, :font, :color, :size)
     # Create a new drawable Text object with the given font and contents.
-    def initialize(font, contents, color = Rubydraw::Color::White, size = 25)
-      @font, @contents, @size, @color = font, contents, size, color
-      @drawable = SDL::TTF.OpenFont(font, size)
+    def initialize(contents, font_name="Times New Roman", color = Rubydraw::Color::White, size = 25)
+      @font, @contents, @size, @color = font_name, contents, size, color
+      if File.exist?(font_name)
+        font_path = font_name
+      else
+        # The font doesn't exist in the program's directory.
+        # Check in Rubydraw's font directory (rubydraw-x.x.x/lib/rubydraw/fonts)
+        font_path = "#{File.dirname(__FILE__)}/../fonts/#{font_name}.ttf"
+        puts File.exists?(font_path)
+      end
+      unless File.exists?(font_path)
+        raise "Font file '#{font_name}' does not exist; attemped to load from '#{font_path}'"
+      end
+      @drawable = SDL::TTF.OpenFont(font_path, size)
       raise(SDLError, "Failed to initialize font: #{SDL.GetError}") if @drawable.pointer.null?
     end
 
